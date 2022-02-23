6faa2034ee554d2dd3b114bb8c4ecca5f9e11ed9
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-02-23 11:47:03 -0500

    checking

--- src/banktransactions/Checking.java
+++ src/banktransactions/Checking.java
@@ -1,9 +1,22 @@
 package banktransactions;
 
 public class Checking extends Account {
+    //so all of the thing we can do in Account are able to be done to checking
 
+    Checking newChecking = new Checking();
+    newChecking.deposit(12.2);
     //extends the Account class
     //includes specific data and operations to a regular checking account
+
+
+
+
+
+
+
+
+
+    //think we're using super.method to use methods from the account class in the checking class
 
 
 }
