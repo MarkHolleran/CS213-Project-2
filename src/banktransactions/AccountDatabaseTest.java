8b148bc37207216470e744d56dfb8b4a95828ab9
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-02-27 11:14:41 -0500

    created junit for other two tests

---
+++ src/banktransactions/AccountDatabaseTest.java
@@ -0,0 +1,16 @@
+package banktransactions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDatabaseTest {
+
+    @Test
+    void open() {
+    }
+
+    @Test
+    void close() {
+    }
+}
