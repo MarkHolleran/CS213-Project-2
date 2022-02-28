806f127ec246ea269456e8de50df211ea2c5f17a
MarkHolleran <97909762+MarkHolleran@users.noreply.github.com>
2022-02-28 16:11:11 -0500

    final version of project

---
+++ src/banktransactions/RunProject2.java
@@ -0,0 +1,17 @@
+package banktransactions;
+
+/**
+ * Class that creates an instance of the Bank teller
+ * class and calls the .run() command to start the program
+ *
+ * @author Mark Holleran, Abhitej Bokka
+ */
+public class RunProject2 {
+
+    public static void main(String[] args) {
+
+        new BankTeller().run();
+
+    }
+
+}
